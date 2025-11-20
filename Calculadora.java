5ccf182fedebd00b5d6ac5473f56b69363bdd076
hectormrtz <4085483@alu.murciaeduca.es>
2025-11-20 22:44:46 +0100

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -8,9 +8,56 @@ package com.mycompany.calculadora;
  *
  * @author kiko
  */
+import java.util.Scanner;
+
+/**
+ *
+ * @author kiko
+ */
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+     
+        System.out.print("Elige la operación (+, -, *, /): ");
+        String operacion = scanner.next();
+
+
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+       
+        double resultado = 0;
+        boolean operacionValida = true;  
+
+        // Realizar la operación
+        if (operacion.equals("+")) {
+            resultado = num1 + num2;
+        } else if (operacion.equals("-")) {
+            resultado = num1 - num2;
+        } else if (operacion.equals("*")) {
+            resultado = num1 * num2;
+        } else if (operacion.equals("/")) {
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                System.out.println("Error: No se puede dividir entre 0");
+                operacionValida = false;
+            }
+        } else {
+            System.out.println("Operación no válida");
+            operacionValida = false;
+        }
+
+    
+        if (operacionValida) {
+            System.out.println("Resultado: " + resultado);
+        }    
+        scanner.close();
     }
 }
